1e43632069173922658845e80ef9ebc90f4a1970
KshitijMagare19 <kshitij.magare19@vit.edu>
2023-07-28 01:35:00 +0530

    Registration frontend part

--- client/src/app/shared/employee.model.ts
+++ client/src/app/shared/employee.model.ts
@@ -1,2 +1,7 @@
 export class Employee {
+    _id : String = "";    
+    fullname: String = "";
+    location: String = "";
+    position: String = "";
+    salary: Number = 0;
 }
--- client/src/app/shared/employee.service.ts
+++ client/src/app/shared/employee.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import {FormBuilder} from '@angular/forms';
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+
+  employeeForm = this.fb.group({
+    _id : [null],    
+    fullname: [''],
+    location: [''],
+    position: [''],
+    salary: [''],
+  })
 }
